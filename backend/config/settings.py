4363f5cab0e1a5fc4b43c3743ee3351393b5560d
Radin <radinam1386@gmail.com>
2026-08-16 01:39:52 -0700

    add apps in backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+
+    'accounts',
+    'classes',
+    'attendance',
+    'exams',
 ]
 
 MIDDLEWARE = [
