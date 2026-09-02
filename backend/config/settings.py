62ee5026ce0643a76eec736ef3b565f5b62edf3a
ARS6666 <arsprogramming123@gmail.com>
2026-09-03 01:07:31 +0330

    khodaya

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,8 +26,11 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "78.157.61.183",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 
