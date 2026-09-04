c20f33e6ed78cf724c2c6e63304c6847e01c9f6c
Radin <radinam1386@gmail.com>
2026-09-03 19:02:46 -0700

    ey

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -134,7 +134,7 @@ export default function Landing() {
                 <span className="typing-cursor">|</span>
               </span>
               <br />
-              رو با ما بسیار حرفه‌ای یاد بگیر
+              رو با ما حرفه‌ای یاد بگیر
             </h1>
 
             <p className="hero-desc reveal" ref={r}>
